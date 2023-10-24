21ff7e44b1f60d70851c24871776853cdc732878
Lucas M. T. Friedrich <frielucas@gmail.com>
2023-10-23 22:53:17 -0300

    commit

---
+++ LeetCode/515/515withBFS.py
@@ -0,0 +1,29 @@
+import math
+import collections
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+  def largestValues(self, root: [TreeNode]) -> [int]:
+    if not root:
+      return []
+
+    ans = []
+    q = collections.deque([root])
+
+    while q:
+      maxi = -math.inf
+      for _ in range(len(q)):
+        root = q.popleft()
+        maxi = max(maxi, root.val)
+        if root.left:
+          q.append(root.left)
+        if root.right:
+          q.append(root.right)
+      ans.append(maxi)
+
+    return ans
